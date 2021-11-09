86ebd4c108ce835661b6487b2098bef54bd876c2
cainpalmer <cain.a.palmer@gmail.com>
2021-11-09 12:39:51 -0500

    about done

--- website/src/App.js
+++ website/src/App.js
@@ -4,7 +4,7 @@ import Home from './components/Home';
 import Portfolio from './components/Portfolio';
 import About from './components/About';
 import IMG_0391 from './images/logo/IMG_0391.png';
-import twitter from './images/socials/twitter.png';
+import facebook from './images/socials/facebook.png';
 import instagram from './images/socials/instagram.png';
 import tiktok from './images/socials/tiktok.png';
 import './App.css';
@@ -15,6 +15,7 @@ const App = () => {
       <Router>
         <div class = 'header'>
           <img class = 'header-image' width = '100px' height = '100px' src = {IMG_0391} alt = 'Hair By Sid Logo' />
+          <input type = 'text' id = 'search' onkeyup = 'searchBar()' placeholder = 'Search...' />
           <nav>
             <a><Link to = '/'>Home</Link></a>
             <a><Link to = '/portfolio'>Portfolio</Link></a>
@@ -35,12 +36,9 @@ const App = () => {
         <div class = 'footer'>
           <span class = 'handles'>
             <h3>Follow Me On:</h3>
-            <img class = 'twitter-png' width = '20px' height = '20px' src = {twitter} alt = 'twitter logo' />
-            <i class="fab fa-twitter">@SidneyW5 </i>
-            <img class = 'twitter-png' width = '20px' height = '20px' src = {instagram} alt = 'instagram logo' />
-            <i class="fab fa-twitter">@SidneyW5 </i>
-            <img class = 'twitter-png' width = '20px' height = '20px' src = {tiktok} alt = 'tiktok logo' />
-            <i class="fab fa-twitter">@SidneyW5 </i>
+            <a href = 'https://www.facebook.com/profile.php?id=100073238490084'><img class = 'facebook-png' width = '20px' height = '20px' src = {facebook} alt = 'facebook logo' /></a>
+            <a href = 'https://www.instagram.com/beauty.sidneyw'><img class = 'twitter-png' width = '20px' height = '20px' src = {instagram} alt = 'instagram logo' /></a>
+            <a href = 'https://www.tiktok.com/'><img class = 'twitter-png' width = '20px' height = '20px' src = {tiktok} alt = 'tiktok logo' /></a>
           </span>
           <nav>
             <a><Link to = '/'>Home</Link></a>
