b659cb8c71f1646e618c944c7139525aea84ed4f
cainpalmer <cain.a.palmer@gmail.com>
2021-11-09 12:50:50 -0500

    fixing unused

--- website/src/components/Home.js
+++ website/src/components/Home.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {BrowserRouter, Link, Route, Routes} from 'react-router-dom';
+import {BrowserRouter, Link} from 'react-router-dom';
 
 function Home() {
     return (
