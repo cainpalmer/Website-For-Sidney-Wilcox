d37f1b0ebb8d79a8146fe95e0836d08b0c50f1ef
cainpalmer <cain.a.palmer@gmail.com>
2021-11-09 04:11:10 -0500

    about page done

--- website/src/App.js
+++ website/src/App.js
@@ -3,18 +3,24 @@ import {BrowserRouter as Router, Switch, Route, Link} from 'react-router-dom';
 import Home from './components/Home';
 import Portfolio from './components/Portfolio';
 import About from './components/About';
+import IMG_0391 from './images/logo/IMG_0391.png';
+import twitter from './images/socials/twitter.png';
+import instagram from './images/socials/instagram.png';
+import tiktok from './images/socials/tiktok.png';
 import './App.css';
 
 const App = () => {
   return (
     <div >
       <Router>
-        <span id = 'nav'>
-        <img src = '../public/logo/IMG_0391.png' alt = 'Hair By Sid Logo'></img>
-          <a><Link to = '/'>Home</Link></a>
-          <a><Link to = '/portfolio'>Portfolio</Link></a>
-          <a><Link to = '/about'>About</Link></a>
-        </span>
+        <div class = 'header'>
+          <img class = 'header-image' width = '100px' height = '100px' src = {IMG_0391} alt = 'Hair By Sid Logo' />
+          <nav>
+            <a><Link to = '/'>Home</Link></a>
+            <a><Link to = '/portfolio'>Portfolio</Link></a>
+            <a><Link to = '/about'>About</Link></a>
+          </nav>
+        </div>
         <Switch>
           <Route exact path = '/'>
             <Home />
@@ -26,12 +32,22 @@ const App = () => {
             <About />
           </Route>
         </Switch>
-        <span id = 'nav'>
-        <img src = '../public/logo/IMG_0391.png' alt = 'Hair By Sid Logo'></img>
-          <a><Link to = '/'>Home</Link></a>
-          <a><Link to = '/portfolio'>Portfolio</Link></a>
-          <a><Link to = '/about'>About</Link></a>
-        </span>
+        <div class = 'footer'>
+          <span class = 'handles'>
+            <h3>Follow Me On:</h3>
+            <img class = 'twitter-png' width = '20px' height = '20px' src = {twitter} alt = 'twitter logo' />
+            <i class="fab fa-twitter">@SidneyW5 </i>
+            <img class = 'twitter-png' width = '20px' height = '20px' src = {instagram} alt = 'instagram logo' />
+            <i class="fab fa-twitter">@SidneyW5 </i>
+            <img class = 'twitter-png' width = '20px' height = '20px' src = {tiktok} alt = 'tiktok logo' />
+            <i class="fab fa-twitter">@SidneyW5 </i>
+          </span>
+          <nav>
+            <a><Link to = '/'>Home</Link></a>
+            <a><Link to = '/portfolio'>Portfolio</Link></a>
+            <a><Link to = '/about'>About</Link></a>
+          </nav>
+        </div>
       </Router>
     </div>
   );
--- website/src/components/About.js
+++ website/src/components/About.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import {BrowserRouter, Link, Route, Routes} from 'react-router-dom';
+import '../App.css';
 
 function About() {
     return (
-        <div>
-            <div>
+        <div class = 'about'>
+            <div class = 'about-h1'>
                 <h1>About Sidney Wilcox</h1>
             </div>
-            <div>
-                <img src = '../../public/logo/IMG_0391.png' alt = 'Sidney Wilcox'></img>
-                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+            <div class = 'about-profile'>
+                <img class = 'about-image' width = '300px' height = '300px' src = 'https://scontent-ort2-2.xx.fbcdn.net/v/t1.6435-9/60865918_536283710241612_8981351475833733120_n.jpg?_nc_cat=103&ccb=1-5&_nc_sid=174925&_nc_ohc=x3CjJe6ulSEAX_kUq3v&_nc_ht=scontent-ort2-2.xx&oh=59d83f5dfe3f561d99ae20d638378b33&oe=61B10DC7' alt = 'Sidney Wilcox' />
+                <p class = 'about-bio'>Hi! My career includes dealing with multiple clients at a time. For the term project I choose to write about one of my new clients that I have just met through a phone call. His name is Steve Chen and he found my advertisement on one of the magazine that I have advertised. At first I knew nothing about him beside that he want to look for a house in San Leandro or San Lorenzo that is 3 bedrooms and 2 bathrooms with a price that is below $250,000. He didn’t even want to give his name out to me….</p>
             </div>
         </div>
     )
