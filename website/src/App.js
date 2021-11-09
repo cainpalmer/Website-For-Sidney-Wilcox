a5ba950c9b6c10d2c9903a92e4543dd48960ea95
cainpalmer <cain.a.palmer@gmail.com>
2021-11-08 22:25:06 -0500

    first commit

---
+++ website/src/App.js
@@ -0,0 +1,14 @@
+import React, {useState, useEffect} from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p></p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
