1b5b936a3442221d4651cc2f14781c3330aac8d1
cainpalmer <cain.a.palmer@gmail.com>
2021-11-09 20:00:16 -0500

    update

--- website/src/components/Home.js
+++ website/src/components/Home.js
@@ -12,13 +12,13 @@ function Home() {
                 <div class = 'option-1'>
                 <h3>An Amazing Experience</h3>
                 <BrowserRouter>
-                    <button><Link to = '/portfolio'>Check out my work!</Link></button>
+                    <button classname = 'move'><Link to = '/portfolio'>Check out my work!</Link></button>
                 </BrowserRouter>
                 </div>
                 <div class = 'option-2'>
                 <h3>My Mission</h3>
                 <BrowserRouter>
-                    <button><Link to = '/about'>Learn about me!</Link></button>
+                    <button classname = 'move'><Link to = '/about'>Learn about me!</Link></button>
                 </BrowserRouter>
                 </div>
             </div>
